df4f7a075f9f7fc8034e435fbcb5ea6e1f8ef4d2
Mohammed Naboulsi <17090882@woodhouse.ac.uk>
2018-11-14 10:40:04 +0000

    Add project files.

---
+++ CA_Testing/MainWindow.xaml.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Class MainWindow
+    Private Sub RadioButton_Checked(sender As Object, e As RoutedEventArgs)
+        Dim LS As LoginScreen
+        Dim connection As New SqlConnection("Server= Moh's PC; Database = LoginDB; Integrated Security = true")
+        Dim command As New SqlCommand("select * from ", connection)
+        Dim adapter As New SqlDataAdapter(command)
+        Dim table As New DataTable()
+        adapter.Fill(table)
+
+
+        LS = New LoginScreen
+        LS.Show()
+        LS = Nothing ' Freeing up memory by setting the form memory value to 0, because if too many forms are open, the program could run out of memory and crash'
+    End Sub
+
+    Private Sub RadioButton_Checked_1(sender As Object, e As RoutedEventArgs)
+        Dim MW As MainWindow
+        MW = New MainWindow
+        Threading.Thread.Sleep(1500)
+        MW.Close()
+        Me.Close()
+    End Sub
+End Class
---
+++ CA_Testing/tyre_stocks_database_plain1DataSet.vb
@@ -0,0 +1,11 @@
+﻿Partial Class tyre_stocks_database_plain1DataSet
+    Partial Public Class StocksDataTable
+        Private Sub StocksDataTable_ColumnChanging(sender As Object, e As System.Data.DataColumnChangeEventArgs) Handles Me.ColumnChanging
+            If (e.Column.ColumnName = Me.IDColumn.ColumnName) Then
+                'Add user code here
+            End If
+
+        End Sub
+
+    End Class
+End Class
